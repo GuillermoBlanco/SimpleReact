7e970a652a3654a58baf30f5ce8904bf84b3a944
Willem <guillermo.blanco.martinez@gmail.com>
2016-09-22 13:39:33 +0200

    Render components on App to pass as Navigation children, test some states and events :gift:

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,19 +1,27 @@
 import React, { Component, PropTypes } from 'react';
 import './Navigation.css';
 
-export default class componentName extends Component {
-    static propTypes = {
-        children: PropTypes.node,
-        logo: PropTypes.string,
-    }
-
-    render() {
-        const { children, logo } = this.props;
-        return (
+export default class Navigation extends Component {
+  static propTypes = {
+    children: PropTypes.node,
+    logo: PropTypes.string,
+  }
+  componentWillMount() {
+    this.setState({
+      // clonedProps: this.props,
+    });
+  }
+  componentDidMount() {
+    console.log('All of this ', this);
+    console.log('State ', this.state);
+  }
+  render() {
+    const { children, logo } = this.props;
+    return (
             <nav className="Navigation">
                 {(logo || null) && <img src={logo} alt="logo" className="Navigation__logo"/>}
                 {children}
             </nav>
         );
-    }
+  }
 }
--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -3,16 +3,56 @@ import './App.css';
 import Navigation from '../Navigation';
 
 export default class App extends Component {
+  static navItems = [
+    {id: 'search', content: 'Search'},
+    {id: 'lifecycle-methods', content: 'Lifecycle Methods'},
+    {id: 'contact', content: 'Contact'},
+  ];
+  constructor(props, context) {
+    super(props, context);
+    this.state = { activeNavItem: 'search', value: '' };
+  }
+
+  _onNavItemClick = (event) => {
+    const id = event.target.id;
+    const { activeNavItem } = this.state;
+
+    if (id !== activeNavItem) {
+      this.setState({ activeNavItem: id });
+    }
+  }
+
+  _renderNavItems() {
+    const navItems = App.navItems;
+    const { activeNavItem } = this.state;
+
+    return navItems.map((navItem, index) => {
+      const { content, id } = navItem;
+      const key = `app-nav-item-${index}`;
+      const className = activeNavItem === id ? 'active' : '';
+
+      return (
+        <li key={key} className={className} id={id} onClick={this._onNavItemClick}>{content}</li>
+      );
+    });
+  }
+
   render() {
+    const { activeNavItem, value } = this.state;
+    console.log('Render app component');
     return (
       <div className="App">
         <Navigation logo="https://facebook.github.io/react/img/logo.svg">
-          <li className="active">Page 1</li>
-          <li>Page 2</li>
-          <li>Page 3</li>
+          {this._renderNavItems()}
         </Navigation>
         <div className="App__content">
-          Here is my content!
+          Filtra en {activeNavItem}
+          <input
+            type="text"
+            value={value}
+            onChange={(event) => this.setState({ value: event.target.value })}
+          />
+          <p>{value}</p>
         </div>
       </div>
     );
