a46b970a0fe4efc65738b1738a790dc86515861d
Willem <guillermo.blanco.martinez@gmail.com>
2016-09-26 15:50:52 +0200

    ES6 transcription

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,8 @@
-export App from './App';
-export Navigation from './Navigation';
-export Course from './Course';
+// ES7 notation:
+// export App from './App';
+// export Navigation from './Navigation';
+// export Course from './Course';
+
+export { default as App } from './App';
+export { default as Navigation } from './Navigation';
+export { default as Course } from './Course';
